af107e2c311ab8bd58641531673e50f1b129282e
chathura2406 <it23689480@my.sliit.lk>
2026-03-19 21:47:50 +0530

    Fix: added frontend correctly

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ padding: "30px", fontFamily: "Arial" }}>
+      <h1>React Frontend Running</h1>
+      <p>This is my Azure deployment lab frontend.</p>
+    </div>
+  );
+}
+
+export default App;
